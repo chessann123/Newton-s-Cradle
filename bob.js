175d7f8365838e5836deb0ef79a273d530e67ec4
chessann123 <55215674+chessann123@users.noreply.github.com>
2020-07-07 19:11:04 -0700

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,25 @@
+class bob {
+    constructor(x, y, r) {
+      var options = {
+          isStatic:false,
+          'restitution':0.8,
+          'friction':0.5,
+          'density':1.2
+      }
+      this.body = Bodies.circle(x, y, r/2, options);
+      this.width = r/2;
+      this.height = r/2;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push ();
+      translate(pos.x,pos.y);
+      rotate (angle);
+      fill ("red");
+      ellipseMode(RADIUS);
+      ellipse(0,0, this.width, this.height);
+      pop();
+    }
+  };
